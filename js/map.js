09a5d5212d72629bb1dad55d4a039c4a5459a880
Louis Liu <pttdog@gmail.com>
2014-04-17 16:14:13 +0800

    export and import files.

--- js/map.js
+++ js/map.js
@@ -13,19 +13,21 @@ var Map = new function CanvasMap(){
       var markers = [];
       var follow = false;
       var posJSON = null;
-      var overlay = null;
-
-      var listMarks = function listMarks(){
-        return [];
-      };
+      var overlayImage = null;
+      var imageLayer = null;
 
       this.reset = function reset(){
+        for(var i = 0; i < markers.length; i++){
+        }
         markers = [];
       };
 
       var getPos = function getPos(){
         var center = map.getCenter();
-        return {'lng':center.lng, 'lat':center.lat, 'zoom': map.getZoom()}
+        var bound = map.getBounds();
+        var sw = bound.getSouthWest();
+        var ne = bound.getNorthEast();
+        return {'lng':center.lng, 'lat':center.lat, 'bound': [[sw.lat, sw.lng], [ne.lat, ne.lng]], 'zoom': map.getZoom()}
       };
       this.getPos = getPos;
 
@@ -36,9 +38,6 @@ var Map = new function CanvasMap(){
         map.scrollWheelZoom.disable();
         map.boxZoom.disable();
         map.keyboard.disable();
-        //map.tap.disable();
-        //map.zoomControl.disable();
-        //map.attributionControl.disable();
       };
 
       this.unfix = function unfix(){
@@ -48,25 +47,34 @@ var Map = new function CanvasMap(){
         map.scrollWheelZoom.enable();
         map.boxZoom.enable();
         map.keyboard.enable();
-        //map.tap.enable();
-        //map.zoomControl.enable();
-        //map.attributionControl.enable();
       };
 
       this.exportData = function exportData(){
-        var pos = getPos();
-        var markersList = listMarks();
-        return 'data:text/json,' + encodeURIComponent(JSON.stringify({'pos': pos, 'markers': markersList}));
+        return 'data:text/json,' + encodeURIComponent(
+            JSON.stringify({'pos': getPos(), 'markers': listMarks()}));
       };
 
-      this.addMarkTo = function addMarkTo(text, x, y){
-        console.log(text+" "+x+" "+y);
-        point = L.containerPointToLatLng(x, y);
-        markers.push({'marker': marker, 'LatLng': point, 'text': text});
+      var addMarkToLatLng = function addMarkToLatLng(text, point){
         marker = L.marker(point).addTo(map);
+        markers.push({'marker': marker, 'LatLng': point, 'text': text});
         marker.on('click', function(e){
           this.bindPop(text).openPopup();
         });
+      }
+
+      this.addMarkTo = function addMarkTo(text, x, y){
+        point = map.containerPointToLatLng([x, y]);
+        addMarkToLatLng(text, point);
+        //point = map.containerPointToLatLng([Math.round(x), Math.round(y)]);
+      };
+
+      var listMarks = function listMarks(){
+        var r = [];
+        for(var i = 0; i < markers.length; i++){
+          var LatLng = [markers[i].LatLng.lat, markers[i].LatLng.lng];
+          r.push({'latlng': LatLng, 'text': markers[i].text});
+        }
+        return r;
       };
 
       this.toggleFollow = function toggleFollow(){
@@ -79,11 +87,23 @@ var Map = new function CanvasMap(){
         }
       };
 
+      var putOverlay = function putOverlay(){
+        imageLayer = L.imageOverlay(overlayImage, posJSON.pos.bound).addTo(map);
+        for(var i = 0; i < posJSON.markers; i++){
+          var point = L.latlng(posJSON.markers[i].latlng);
+          addMarkToLatLng(posJSON.markers[i].text, point);
+        }
+        document.getElementById('Canvas').setAttribute('class', 'pointerDisable');
+        document.getElementById('Map').setAttribute('class', 'pointerEnabel');
+        map.setView([posJSON.pos.lat, posJSON.pos.lng], posJSON.pos.zoom - 1, {'reset': true});
+        Map.unfix();
+      };
+
       this.loadPos = function loadPos(file){
         var reader = new FileReader();
         reader.onload = function(e){
           posJSON = JSON.parse(reader.result);
-          if(overlay){
+          if(overlayImage){
             putOverlay();
           }
         };
@@ -93,11 +113,11 @@ var Map = new function CanvasMap(){
       this.loadImg = function loadImg(file){
         var reader = new FileReader();
         reader.onload = function(e){
-          overlay = reder.result;
+          overlayImage = reader.result;
           if(posJSON){
             putOverlay();
           }
         }
-        reader.readAsDataURL();
+        reader.readAsDataURL(file);
       }
 };
